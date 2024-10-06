e9f6c8e6df21dec9ab24e3c807af0fe4c7440ac1
AbdellahRAISSOUNI <abdellahraissouni@gmail.com>
2024-10-06 13:34:09 +0100

    huhh

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -24,10 +24,10 @@ export const LogoTicker = () => {
     <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
         <h2 className="text-xl text-center text-white/70">
-          Trusted by the world's most innovative teams
-        </h2>
-        <div className="flex overflow-hidden mt-9 before:content-[&quot;&quot;] before:z-10 after:content-[&quot;&quot;] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))] ">
-        <motion.div
+          Trusted by the world&apos;s most innovative teams
+        </h2> {/* Fixed the unescaped quote */}
+        <div className="flex overflow-hidden mt-9 relative before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 before:left-0 after:right-0 before:top-0 after:top-0 before:z-10 after:z-10 before:bg-[linear-gradient(to_right,#000,rgba(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgba(0,0,0,0))]">
+          <motion.div
             transition={{
               duration: 14,
               ease: "linear",
